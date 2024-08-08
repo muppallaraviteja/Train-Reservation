9d8ef3c2b489243810363f6ef248776256b44bf7
Ravi Teja muppalla <153738634+bonitoravi@users.noreply.github.com>
2024-08-08 10:48:18 +0530

    Added the empty project

---
+++ src/test/java/com/ravi/trainReservation/TrainReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi.trainReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
